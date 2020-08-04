2887f402df24c009cd8b0a3cf9d94256dbf3ebaf
beyondsundae <p00000rice@gmail.com>
2020-08-04 23:32:17 +0700

    Updates

---
+++ precache-manifest.66185c404aa3f17eda794929ecefd7dc.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc43f6574dd7dbb5409d59f9835e341b",
+    "url": "/sectioncovid/index.html"
+  },
+  {
+    "revision": "4ff70d912720ee43a50d",
+    "url": "/sectioncovid/static/css/main.dd09f585.chunk.css"
+  },
+  {
+    "revision": "933af004fab3c085e51f",
+    "url": "/sectioncovid/static/js/2.e0225feb.chunk.js"
+  },
+  {
+    "revision": "a835da89980c4c418d747aadca753d71",
+    "url": "/sectioncovid/static/js/2.e0225feb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ff70d912720ee43a50d",
+    "url": "/sectioncovid/static/js/main.ffaef568.chunk.js"
+  },
+  {
+    "revision": "4d3df09ac8c73365992e",
+    "url": "/sectioncovid/static/js/runtime-main.fe138be3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sectioncovid/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "3ab1e1b825cf159ed3c0bc44089ae512",
+    "url": "/sectioncovid/static/media/virus icon.3ab1e1b8.svg"
+  },
+  {
+    "revision": "f9ec0d6fb4a261f5909548ed232016e2",
+    "url": "/sectioncovid/static/media/virus-bg.f9ec0d6f.svg"
+  },
+  {
+    "revision": "cd89a91cd33a9d86a666ab743618e283",
+    "url": "/sectioncovid/static/media/virus-main.cd89a91c.svg"
+  },
+  {
+    "revision": "283be8c5cd4c3ce76a1c5058aff9204c",
+    "url": "/sectioncovid/static/media/virus.283be8c5.svg"
+  }
+]);
